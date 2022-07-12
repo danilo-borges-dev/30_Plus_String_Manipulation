0b842f6453b8b0654be50760b225d12e605a948d
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-07-12 10:59:08 -0300

    Entendo ToUpper ToLower com strings

--- StringManipulation/StringManipulation/Program.cs
+++ StringManipulation/StringManipulation/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Globalization;
+
+Main();
+
+static void Main()
+{
+    StringConversion();
+}
+
+static void StringConversion()
+{
+    string testString = "This is the FBI Calling!";
+    TextInfo currentTextInfo = CultureInfo.CurrentCulture.TextInfo;
+    TextInfo russTextInfo = new CultureInfo("pt-br", false).TextInfo;
+    string result;
+
+    result = testString.ToLower();
+    Console.WriteLine(result);
+
+    result = testString.ToUpper();
+    Console.WriteLine(result);
+
+    result = currentTextInfo.ToTitleCase(testString);
+    Console.WriteLine(result);
+
+    result = russTextInfo.ToTitleCase(testString);
+    Console.WriteLine(result);
+
+}
